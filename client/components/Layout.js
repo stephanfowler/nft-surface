a5f7eb96fe423f9664200baada80887b6c6fcfe2
Stephan Fowler <stephanfowler@gmail.com>
2021-11-15 10:42:19 +0000

    favicons etc

--- client/components/Layout.js
+++ client/components/Layout.js
@@ -13,7 +13,11 @@ export default function Layout({ children, home, nft, chainId }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <title key="title">{process.env.siteTitle}</title>
         <meta name="description" content={process.env.siteDescription} />
-        <link rel="icon" href="/favicon.ico" />
+        
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <meta name="twitter:card" content="summary_large_image" key="twcard" />
         <meta name="twitter:creator" content={process.env.twitterHandle} key="twhandle" />
