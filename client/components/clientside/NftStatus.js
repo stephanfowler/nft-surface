f8d6a634299e87ed3a801605c862b32df02bb6df
Stephan Fowler <stephanfowler@gmail.com>
2021-11-23 13:18:11 +0000

    refactor

--- client/components/clientside/NftStatus.js
+++ client/components/clientside/NftStatus.js
@@ -1,217 +1,180 @@
 import { ethers } from "ethers";
 import { useEffect, useState } from "react";
 
-import { 
-  networkName,
-  isTransactionMined,
-  getWallet,
-  contractCall_ownerOf,
-  contractCall_mintable,
-  contractCall_mint
+import {
+	networkName,
+	isTransactionMined,
+	getWallet,
+	contractCall_ownerOf,
+	contractCall_mintable,
+	contractCall_mint
 } from "@utils/ethereum-interact.js";
 
 import {
-  etherscanAddressLink,
-  etherscanTxLink,
-  etherscanBlockLink
+	etherscanAddressLink,
+	etherscanTxLink
 } from "@utils/links.js"
 
+import Minter from '@components/clientside/Minter'
 import SalesForm from '@components/clientside/SalesForm'
 
 import styles from '@components/Nft.module.css'
 
 const NftStatus = ({ nft, chainId, status, setStatus }) => {
-  const [walletAddress, setWallet] = useState();
-  const [statusUpdated, setStatusUpdated] = useState();
-  const [owner, setOwner] = useState();
-  const [tx, setTx] = useState();
-  const [isConnecting, setIsConnecting] = useState();
-  const [chainIdMismatch, setChainIdMismatch] = useState();
-  const [notify, setNotify] = useState();
+	const [walletAddress, setWallet] = useState();
+	const [statusUpdated, setStatusUpdated] = useState();
+	const [owner, setOwner] = useState();
+	const [tx, setTx] = useState();
+	const [isConnecting, setIsConnecting] = useState();
+	const [chainIdMismatch, setChainIdMismatch] = useState();
+	const [notify, setNotify] = useState();
 
-  const contractAddress = nft.metadata.contractAddress;
+	const contractAddress = nft.metadata.contractAddress;
 
-  useEffect(() => {
-    fetchWallet();
+	useEffect(() => {
+		fetchWallet();
 
-    async function updateTokenStatus() {
-      const _owner = await contractCall_ownerOf(nft, contractAddress, chainId);
-      if (_owner) {
-        setStatus("minted")
-        setOwner(_owner);
-      } else {
-          await contractCall_mintable(nft, contractAddress, chainId) ?
-            setStatus("mintable_confirmed") :
-            setStatus("burntOrRevoked")
-      }
-      setStatusUpdated(true);
-    }
-    updateTokenStatus();
+		async function updateTokenStatus() {
+			const _owner = await contractCall_ownerOf(nft, contractAddress, chainId);
+			if (_owner) {
+				setOwner(_owner);
+			} else {
+				await contractCall_mintable(nft, contractAddress, chainId) ?
+					setStatus("mintable_confirmed") :
+					setStatus("burntOrRevoked")
+			}
+			setStatusUpdated(true);
+		}
+		updateTokenStatus();
 
-    async function addWalletListener() {
-      if (window.ethereum) {
-        window.ethereum.on("accountsChanged", () => {
-          fetchWallet();
-          updateTokenStatus();
-        });
-        window.ethereum.on("chainChanged", () => {
-          window.location.reload();
-        });
-      }
-    }
-    addWalletListener();
+		async function addWalletListener() {
+			if (window.ethereum) {
+				window.ethereum.on("accountsChanged", () => {
+					fetchWallet();
+					updateTokenStatus();
+				});
+				window.ethereum.on("chainChanged", () => {
+					window.location.reload();
+				});
+			}
+		}
+		addWalletListener();
 
-    return () => {
-      setStatus();
-      setOwner();
-      setWallet();
-    }
-  }, []);
+		return () => {
+			setStatus();
+			setOwner();
+			setWallet();
+		}
+	}, []);
 
-  async function fetchWallet(isConnect) {
-    const { address, walletChainId, error } = await getWallet(isConnect);
-    if (walletChainId && walletChainId !== chainId) {
-      setChainIdMismatch(true);
-    } else if (address) {
-      setWallet(address);
-    } else {
-      setNotify(error);
-    }
-  }
+	async function fetchWallet(isConnect) {
+		const { address, walletChainId, error } = await getWallet(isConnect);
+		if (walletChainId && walletChainId !== chainId) {
+			setChainIdMismatch(true);
+		} else if (address) {
+			setWallet(address);
+		} else {
+			setNotify(error);
+		}
+	}
 
-  const doConnectWallet = async (e) => {
-    e && e.preventDefault();
-    await fetchWallet(true)
-  };
+	const doConnectWallet = async (e) => {
+		e && e.preventDefault();
+		await fetchWallet(true)
+	};
 
-  const doMint = async (e) => {
-    if (!window.ethereum) return;
-    setNotify("confirmation_required");
-    setIsConnecting(true);
-    if (!walletAddress) await doConnectWallet();
-    try {  
-      const { tx, error } = await contractCall_mint(nft, contractAddress, chainId);
-      if (tx) {
-        setTx(tx);
-        setNotify("tx_pending");
-        const txReceipt = await isTransactionMined(tx.hash, chainId)
-        if (txReceipt) {
-          setNotify("tx_succeded");
-          setOwner(walletAddress);
-          setStatus("minted");
-        } else {
-          setNotify("tx_failed");
-        }
-      } else {
-        setNotify(error.includes("insufficient funds") ? "insufficient_funds" : error);
-      }
-    } catch(error) {
-      setNotify(error.message);
-    }
-    setIsConnecting(false);
-  };
+	return (
+		chainIdMismatch ?
+			<div className={styles.notification}>
+				{"To establish the status of this NFT, please switch your wallet to network: "}{networkName(chainId)}
+			</div>
 
-  return (
-    chainIdMismatch ?
-      <div className={styles.notification}>
-        {"To establish the status of this NFT, please switch your wallet to network: "}{networkName(chainId)}
-      </div>
+			: !statusUpdated ?
+				<div className={styles.minter}>
+					Checking NFT status …
+				</div>
 
-    : !statusUpdated ?
-      <div className={styles.minter}>
-        Checking NFT status …
-      </div>
+				:
+				<div className={styles.minter_updated}>
+					{owner &&
+						<SalesForm
+							nft={nft}
+							owner={owner}
+							doConnectWallet={doConnectWallet}
+							walletAddress={walletAddress}
+							setOwner={setOwner}
+							setNotify={setNotify}
+							setTx={setTx}
+							contractAddress={contractAddress}
+							chainId={chainId} />
+					}
 
-    :
-    <div className={styles.minter_updated}>
-      {owner && (
-        <SalesForm
-          nft={nft}
-          owner={owner}
-          doConnectWallet={doConnectWallet}
-          walletAddress={walletAddress}
-          setOwner={setOwner}
-          setNotify={setNotify}
-          setTx={setTx}          
-          contractAddress={contractAddress}
-          chainId={chainId} />
-      )}
+					{!owner && status === "mintable_confirmed" && 
+						<Minter
+							nft={nft}
+							doConnectWallet={doConnectWallet}
+							walletAddress={walletAddress}
+							setOwner={setOwner}
+							setNotify={setNotify}
+							setTx={setTx}
+							contractAddress={contractAddress}
+							chainId={chainId} />
+					}
 
-      {status === "mintable_confirmed" && (
-        <>
-          <div>
-            <div>This NFT is available for minting</div>
-            <img  className={styles.ethereumLogo} src="/ethereum.svg" />
-            <span className={styles.nftPriceETH}>
-              {nft.weiPrice === "0" ?
-                "FREE" :
-                `${ethers.utils.formatEther(nft.weiPrice)} ETH`
-              }
-            </span>
-            <span className={styles.nftPriceGas}>{" + gas fee"}</span>
-          </div>
-          <div id="walletActions">
-              <button onClick={doMint} disabled={isConnecting || !window.ethereum}>
-                Mint this NFT
-              </button>
-          </div>
-        </>
-      )}
+					{status === "withheld" && (
+						<div>This NFT is reserved. Please contact the artist.</div>
+					)}
 
-      {status === "withheld" && (
-        <div>This NFT is reserved. Please contact the artist.</div>
-      )}  
+					{status === "burntOrRevoked" && (
+						<div>Sorry, this NFT has been burnt or revoked.</div>
+					)}
 
-      {status === "burntOrRevoked" && (
-        <div>Sorry, this NFT has been burnt or revoked.</div>
-      )}
+					<div className={styles.notification}>
+						{notify === "insufficient_funds" ?
+							<div>You have insufficient funds in your wallet</div>
 
-      <div className={styles.notification}>
-        { notify === "insufficient_funds" ?
-          <div>You have insufficient funds in your wallet</div>
+							: notify === "tx_pending" ?
+								<div>{"Please be patient while transaction "}{etherscanTxLink(tx.hash)}{" is added to the blockchain..."}</div>
 
-        : notify === "tx_pending" ?
-        <div>{"Please be patient while transaction "}{etherscanTxLink(tx.hash)}{" is added to the blockchain..."}</div>
-        
-        : notify === "tx_succeded" ?
-        <div>{"Done! Transaction "}{etherscanTxLink(tx.hash)}{" was succesful"}</div>
-          
-        : notify === "tx_failed" ?
-        <div>{"Sorry, transaction "}{etherscanTxLink(tx.hash)}{" failed"}</div>
-          
-        : notify === "confirmation_required" ?
-        <div>{"Please confirm using your wallet..."}</div>
-          
-        : notify === "wallet_unavailable" ?
-        <div>
-          <div>
-            To mint or buy NFTs you need an Ethereum wallet
-          </div>
-          <div>
-            On mobile use the <a href={`https://metamask.io/`}>Metamask</a> app broswer to view this website
-          </div>
-          <div>
-            On desktop enable the <a href={`https://metamask.io/`}>Metamask</a> browser extension
-          </div>
-        </div>
-          
-        : notify ?
-        <div>{notify}</div>
-          
-        : walletAddress ?
-          <div>Your wallet address is{" "}{etherscanAddressLink(walletAddress)}</div>
+								: notify === "tx_succeded" ?
+									<div>{"Done! Transaction "}{etherscanTxLink(tx.hash)}{" was succesful"}</div>
 
-        : window.ethereum ?
-          <div>
-            {"Connect your "} 
-            <a href="" onClick={doConnectWallet}>Ethereum wallet</a>
-          </div>
+									: notify === "tx_failed" ?
+										<div>{"Sorry, transaction "}{etherscanTxLink(tx.hash)}{" failed"}</div>
 
-        : <></>
-        }
-       </div>
-    </div>
-  );
+										: notify === "confirmation_required" ?
+											<div>{"Please confirm using your wallet..."}</div>
+
+											: notify === "wallet_unavailable" ?
+												<div>
+													<div>
+														To mint or buy NFTs you need an Ethereum wallet
+													</div>
+													<div>
+														On mobile use the <a href={`https://metamask.io/`}>Metamask</a> app broswer to view this website
+													</div>
+													<div>
+														On desktop enable the <a href={`https://metamask.io/`}>Metamask</a> browser extension
+													</div>
+												</div>
+
+												: notify ?
+													<div>{notify}</div>
+
+													: walletAddress ?
+														<div>Your wallet address is{" "}{etherscanAddressLink(walletAddress)}</div>
+
+														: window.ethereum ?
+															<div>
+																{"Connect your "}
+																<a href="" onClick={doConnectWallet}>Ethereum wallet</a>
+															</div>
+
+															: <></>
+						}
+					</div>
+				</div>
+	);
 };
 
 export default NftStatus;
--- client/components/clientside/SalesForm.js
+++ client/components/clientside/SalesForm.js
@@ -2,231 +2,231 @@ import { useState, useEffect } from "react";
 import { ethers } from "ethers";
 
 import Link from 'next/link'
-import { 
-    isTransactionMined,
-    contractCall_price,
-    contractCall_setPrice,
-    contractCall_buy
-  } from "@utils/ethereum-interact.js";
+import {
+	isTransactionMined,
+	contractCall_price,
+	contractCall_setPrice,
+	contractCall_buy
+} from "@utils/ethereum-interact.js";
 
 import {
-    etherscanAddressLink
+	etherscanAddressLink
 } from "@utils/links.js"
-  
+
 import styles from '@components/Nft.module.css'
 
-export default function SalesForm({ 
-    nft,
-    owner,
-    doConnectWallet,
-    walletAddress,
-    setOwner,
-    setNotify,
-    setTx, 
-    contractAddress,
-    chainId }) {
+export default function SalesForm({
+	nft,
+	owner,
+	doConnectWallet,
+	walletAddress,
+	setOwner,
+	setNotify,
+	setTx,
+	contractAddress,
+	chainId }) {
 
-    const [price, setPrice] = useState();
-    const [priceETH, setPriceETH] = useState();
-    const [displayPriceETH, setDisplayPriceETH] = useState();
-    const [expanded, setExpanded] = useState(false);
-    const [connecting, setConnecting] = useState();
-    const [forceRender, doForceRender] = useState();
+	const [price, setPrice] = useState();
+	const [priceETH, setPriceETH] = useState();
+	const [displayPriceETH, setDisplayPriceETH] = useState();
+	const [expanded, setExpanded] = useState(false);
+	const [connecting, setConnecting] = useState();
+	const [render, forceRender] = useState();
 
-    const userIsOwner = (owner && walletAddress && (owner.toUpperCase() === walletAddress.toUpperCase()));
+	const userIsOwner = (owner && walletAddress && (owner.toUpperCase() === walletAddress.toUpperCase()));
 
-    useEffect(() => {
-        async function getPrice() {
-            const _price = await contractCall_price(nft, contractAddress, chainId);
-            setPrice(_price);
-            const _priceETH = ethers.utils.formatEther(_price);
-            setPriceETH(_priceETH);
-            setDisplayPriceETH(_priceETH);
-        }
-        getPrice();
+	useEffect(() => {
+		async function getPrice() {
+			const _price = await contractCall_price(nft, contractAddress, chainId);
+			setPrice(_price);
+			const _priceETH = ethers.utils.formatEther(_price);
+			setPriceETH(_priceETH);
+			setDisplayPriceETH(_priceETH);
+		}
+		getPrice();
 
-        return () => {
-            setPrice();
-            setPriceETH();
-            setDisplayPriceETH();
-            setExpanded();
-            setConnecting();
-        };
-    }, [forceRender]);
+		return () => {
+			setPrice();
+			setPriceETH();
+			setDisplayPriceETH();
+			setExpanded();
+			setConnecting();
+		};
+	}, [render]);
 
-    const cancel = (evt) => {
-        evt.preventDefault();
-        setExpanded(false);
-        setDisplayPriceETH(priceETH);
-    }
+	const cancel = (evt) => {
+		evt.preventDefault();
+		setExpanded(false);
+		setDisplayPriceETH(priceETH);
+	}
 
-    const submit = (evt) => {
-        evt.preventDefault();
-        doUpdate(displayPriceETH)
-    }
+	const submit = (evt) => {
+		evt.preventDefault();
+		doUpdate(displayPriceETH)
+	}
 
-    const setZero = (evt) => {
-        evt.preventDefault();
-        setDisplayPriceETH("0")
-        doUpdate("0");
-    }
+	const setZero = (evt) => {
+		evt.preventDefault();
+		setDisplayPriceETH("0")
+		doUpdate("0");
+	}
 
-    const expand = (evt) => {
-        evt.preventDefault();
-        setExpanded(true);
-    }
+	const expand = (evt) => {
+		evt.preventDefault();
+		setExpanded(true);
+	}
 
-    const marketplaces = () => {
-        return <span className={styles.marketplaces}>
-            <Link href={nft.openseaAsset}><a className={styles.nftMarket}>OpenSea</a></Link>
-            {" · "}
-            <Link href={nft.raribleAsset}><a className={styles.nftMarket}>Rarible</a></Link>
-        </span>
-    }
+	const marketplaces = () => {
+		return <span className={styles.marketplaces}>
+			<Link href={nft.openseaAsset}><a className={styles.nftMarket}>OpenSea</a></Link>
+			{" · "}
+			<Link href={nft.raribleAsset}><a className={styles.nftMarket}>Rarible</a></Link>
+		</span>
+	}
 
-    const doUpdate = async (newPriceETH) => {
-        newPriceETH = newPriceETH || '0';
-        if (newPriceETH != priceETH) {
-            const newPrice = ethers.utils.parseEther(newPriceETH);
-            setNotify("confirmation_required");
-            setConnecting(true);
-            try {
-                const { tx, error } = await contractCall_setPrice(nft, newPrice, contractAddress, chainId);
-                if (tx) {
-                    setTx(tx);
-                    setNotify("tx_pending");
-                    const txReceipt = await isTransactionMined(tx.hash, chainId)
-                    if (txReceipt) {
-                        setNotify("tx_succeded")
-                        setPrice(newPrice);
-                        setDisplayPriceETH(newPriceETH);
-                        doForceRender(newPrice);
-                    } else {
-                        setNotify("tx_failed");
-                    }
-                } else {
-                    setNotify(error.includes("insufficient funds") ? "insufficient_funds" : error);
-                }
-            } catch(error) {
-                setNotify(error.message);
-            }
-            setExpanded(false);
-            setConnecting(false);
-        }
-    }
+	const doUpdate = async (newPriceETH) => {
+		newPriceETH = newPriceETH || '0';
+		if (newPriceETH != priceETH) {
+			const newPrice = ethers.utils.parseEther(newPriceETH);
+			setNotify("confirmation_required");
+			setConnecting(true);
+			try {
+				const { tx, error } = await contractCall_setPrice(nft, newPrice, contractAddress, chainId);
+				if (tx) {
+					setTx(tx);
+					setNotify("tx_pending");
+					const txReceipt = await isTransactionMined(tx.hash, chainId)
+					if (txReceipt) {
+						setNotify("tx_succeded")
+						setPrice(newPrice);
+						setDisplayPriceETH(newPriceETH);
+						forceRender(newPrice);
+					} else {
+						setNotify("tx_failed");
+					}
+				} else {
+					setNotify(error.includes("insufficient funds") ? "insufficient_funds" : error);
+				}
+			} catch (error) {
+				setNotify(error.message);
+			}
+			setExpanded(false);
+			setConnecting(false);
+		}
+	}
 
-    const doBuy = async (e) => {
-        if (!window.ethereum) return;
-        setNotify("confirmation_required");
-        setConnecting(true);
-        if (!walletAddress) await doConnectWallet();
-        if (walletAddress === owner) return;
-        try {
-            const { tx, error } = await contractCall_buy(nft, price, contractAddress, chainId);
-            if (tx) {
-                setTx(tx);
-                setNotify("tx_pending");
-                const txReceipt = await isTransactionMined(tx.hash, chainId)
-                if (txReceipt) {
-                    setNotify("tx_succeded");
-                    setOwner(walletAddress);
-                    doForceRender(1);
-                } else {
-                    setNotify("tx_failed");
-                }
-            } else {
-                setNotify(error.includes("insufficient funds") ? "insufficient_funds" : error);
-            }
-        } catch(error) {
-            setNotify(error.message);
-        }
-        setConnecting(false);
-    };
+	const doBuy = async (e) => {
+		if (!window.ethereum) return;
+		setNotify("confirmation_required");
+		setConnecting(true);
+		if (!walletAddress) await doConnectWallet();
+		if (walletAddress === owner) return;
+		try {
+			const { tx, error } = await contractCall_buy(nft, price, contractAddress, chainId);
+			if (tx) {
+				setTx(tx);
+				setNotify("tx_pending");
+				const txReceipt = await isTransactionMined(tx.hash, chainId)
+				if (txReceipt) {
+					setNotify("tx_succeded");
+					setOwner(walletAddress);
+					forceRender(1);
+				} else {
+					setNotify("tx_failed");
+				}
+			} else {
+				setNotify(error.includes("insufficient funds") ? "insufficient_funds" : error);
+			}
+		} catch (error) {
+			setNotify(error.message);
+		}
+		setConnecting(false);
+	};
 
-    return (
-        <div>
-            {userIsOwner && (
-                <div className={styles.nftOwner}>
-                This NFT is owned by{" "}{etherscanAddressLink(owner, "you")}
-                </div>
-            )}
+	return (
+		<div>
+			{userIsOwner && (
+				<div className={styles.nftOwner}>
+					This NFT is owned by{" "}{etherscanAddressLink(owner, "you")}
+				</div>
+			)}
 
-            {!userIsOwner && (
-                <div className={styles.nftOwner}>
-                This NFT is owned by {" "}{etherscanAddressLink(owner)}
-                </div>
-            )}
+			{!userIsOwner && (
+				<div className={styles.nftOwner}>
+					This NFT is owned by {" "}{etherscanAddressLink(owner)}
+				</div>
+			)}
 
-            {userIsOwner && expanded &&
-                <form>
-                    {"Sell this NFT for "}
-                    <input disabled={connecting}
-                        autoFocus
-                        type="string"
-                        value={displayPriceETH}
-                        onChange={e => setDisplayPriceETH(e.target.value)}
-                        />
-                    {" ETH "}
-                    <div className={styles.formActions}>
-                        <button className={styles.buttony} onClick={submit} disabled={connecting || parseFloat(priceETH) === parseFloat(displayPriceETH)}>OK</button>
-                        {parseFloat(priceETH) > 0 &&
-                            <button className={styles.buttony} onClick={setZero} disabled={connecting}>Terminate sale</button>
-                        }
-                        <button className={styles.buttony} onClick={cancel} disabled={connecting}>Cancel</button>
-                    </div>
-                </form>
-            }
+			{userIsOwner && expanded &&
+				<form>
+					{"Sell this NFT for "}
+					<input disabled={connecting}
+						autoFocus
+						type="string"
+						value={displayPriceETH}
+						onChange={e => setDisplayPriceETH(e.target.value)}
+					/>
+					{" ETH "}
+					<div className={styles.formActions}>
+						<button className={styles.buttony} onClick={submit} disabled={connecting || parseFloat(priceETH) === parseFloat(displayPriceETH)}>OK</button>
+						{parseFloat(priceETH) > 0 &&
+							<button className={styles.buttony} onClick={setZero} disabled={connecting}>Terminate sale</button>
+						}
+						<button className={styles.buttony} onClick={cancel} disabled={connecting}>Cancel</button>
+					</div>
+				</form>
+			}
 
-            {userIsOwner && !expanded && priceETH == 0  &&
-                <div>
-                    <button onClick={expand}>
-                      Sell this NFT
-                    </button>
-                    {" or trade it on "}
-                    {marketplaces()}
-                </div>
-            }
+			{userIsOwner && !expanded && priceETH == 0 &&
+				<div>
+					<button onClick={expand}>
+						Sell this NFT
+					</button>
+					{" or trade it on "}
+					{marketplaces()}
+				</div>
+			}
 
-            {userIsOwner && !expanded && priceETH > 0 &&
-                <>
-                    <img  className={styles.ethereumLogo} src="/ethereum.svg" />
-                    <span className={styles.nftPriceETH}>
-                        {priceETH}{" ETH"}
-                    </span>
-                    <span className={styles.nftPriceGas}>{" + gas fee"}</span>
-                    <form>
-                        <button className={styles.buttony} onClick={expand}>Amend price or terminate sale</button>
-                    </form>
-                    <div>
-                        {"You can also sell on "}
-                        {marketplaces()}
-                    </div>
-                </>
-            }
+			{userIsOwner && !expanded && priceETH > 0 &&
+				<>
+					<img className={styles.ethereumLogo} src="/ethereum.svg" />
+					<span className={styles.nftPriceETH}>
+						{priceETH}{" ETH"}
+					</span>
+					<span className={styles.nftPriceGas}>{" + gas fee"}</span>
+					<form>
+						<button className={styles.buttony} onClick={expand}>Amend price or terminate sale</button>
+					</form>
+					<div>
+						{"You can also sell on "}
+						{marketplaces()}
+					</div>
+				</>
+			}
 
-            {!userIsOwner && priceETH == 0 &&
-                <div>
-                    {"Make an offer on "}
-                    {marketplaces()}
-                </div>
-            }
+			{!userIsOwner && priceETH == 0 &&
+				<div>
+					{"Make an offer on "}
+					{marketplaces()}
+				</div>
+			}
 
-            {!userIsOwner && priceETH > 0 &&
-                <div>
-                    <img  className={styles.ethereumLogo} src="/ethereum.svg" />
-                    <span className={styles.nftPriceETH}>
-                        {priceETH}{" ETH"}
-                    </span>
-                    <span className={styles.nftPriceGas}>{" + gas fee"}</span>
-                    <button onClick={doBuy} disabled={connecting || !window.ethereum} >
-                      Buy this NFT
-                    </button>
-                    <div>
-                        {"or make an offer on "} 
-                        {marketplaces()}
-                    </div>
-                </div>
-            }
-        </div>
-    );
+			{!userIsOwner && priceETH > 0 &&
+				<div>
+					<img className={styles.ethereumLogo} src="/ethereum.svg" />
+					<span className={styles.nftPriceETH}>
+						{priceETH}{" ETH"}
+					</span>
+					<span className={styles.nftPriceGas}>{" + gas fee"}</span>
+					<button onClick={doBuy} disabled={connecting || !window.ethereum} >
+						Buy this NFT
+					</button>
+					<div>
+						{"or make an offer on "}
+						{marketplaces()}
+					</div>
+				</div>
+			}
+		</div>
+	);
 }
