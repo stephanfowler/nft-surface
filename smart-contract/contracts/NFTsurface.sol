52e598ffc41401b94eb534284a4c63d2edadb3ee
Stephan Fowler <stephanfowler@gmail.com>
2022-01-10 14:10:32 +0000

    lint contract

--- smart-contract/contracts/NFTsurface.sol
+++ smart-contract/contracts/NFTsurface.sol
@@ -13,7 +13,13 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
  *  @notice ERC721 contract for stand-alone NFT collections with separable "agent" role, payment splitting, and lazy-minting capability
  *  @dev Enables lazy-minting by any user via precomputed signatures
  */
-contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSplitter {
+contract NFTsurface is
+    ERC721,
+    ERC721Burnable,
+    EIP712,
+    AccessControl,
+    PaymentSplitter
+{
     event IdRevoked(uint256 tokenId);
     event IdFloorSet(uint256 idFloor);
 
@@ -21,7 +27,7 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
 
     // See NFT Provenance notes regarding why these are immutable
     address public immutable owner;
-    uint16  public immutable royaltyBasisPoints;
+    uint16 public immutable royaltyBasisPoints;
 
     uint256 public totalSupply;
     uint256 public idFloor;
@@ -30,7 +36,7 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
     mapping(uint256 => uint256) private prices;
 
     /**
-     *  @dev Constructor immutably sets "owner" to the message sender; be sure to deploy contract using the account of the creator/artist/brand/etc. 
+     *  @dev Constructor immutably sets "owner" to the message sender; be sure to deploy contract using the account of the creator/artist/brand/etc.
      *  @param name ERC721 token name
      *  @param symbol ERC721 token symbol
      *  @param admin The administrator address can reassign roles
@@ -47,8 +53,8 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
         address[] memory payees,
         uint256[] memory shares,
         uint16 royaltyBasisPoints_
-    ) 
-        ERC721(name, symbol) 
+    )
+        ERC721(name, symbol)
         EIP712("NFTsurface", "1.0.0")
         PaymentSplitter(payees, shares)
     {
@@ -57,7 +63,7 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
         _setupRole(AGENT_ROLE, agent);
     }
-    
+
     event PriceSet(uint256 id, uint256 price);
     event Bought(uint256 id, address buyer);
 
@@ -67,7 +73,11 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
      *  @param id The intended token Id
      *  @param uri The intended token URI
      */
-    function mintAuthorized(address recipient, uint256 id, string memory uri) external {
+    function mintAuthorized(
+        address recipient,
+        uint256 id,
+        string memory uri
+    ) external {
         require(hasRole(AGENT_ROLE, _msgSender()), "unauthorized to mint");
         require(vacant(id));
         _mint(recipient, id, uri);
@@ -80,7 +90,11 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
      *  @param uri The intended token URI
      *  @param signature The ERC712 signature of the hash of message value, id, and uri
      */
-    function mint(uint256 id, string memory uri, bytes calldata signature) external payable {
+    function mint(
+        uint256 id,
+        string memory uri,
+        bytes calldata signature
+    ) external payable {
         require(mintable(msg.value, id, uri, signature));
         _mint(_msgSender(), id, uri);
     }
@@ -93,9 +107,20 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
      *  @param uri The intended token URI
      *  @param signature The ERC712 signature of the hash of weiPrice, id, and uri
      */
-    function mintable(uint256 weiPrice, uint256 id, string memory uri, bytes calldata signature) public view returns (bool) {
+    function mintable(
+        uint256 weiPrice,
+        uint256 id,
+        string memory uri,
+        bytes calldata signature
+    ) public view returns (bool) {
         require(vacant(id));
-        require(hasRole(AGENT_ROLE, ECDSA.recover(_hash(weiPrice, id, uri), signature)), 'signature invalid or signer unauthorized');
+        require(
+            hasRole(
+                AGENT_ROLE,
+                ECDSA.recover(_hash(weiPrice, id, uri), signature)
+            ),
+            "signature invalid or signer unauthorized"
+        );
         return true;
     }
 
@@ -104,7 +129,7 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
      *  @dev Reverts if the Id is previously minted, revoked, or burnt
      *  @param id The token Id
      */
-    function vacant(uint256 id) public view returns(bool) {
+    function vacant(uint256 id) public view returns (bool) {
         require(!_exists(id), "tokenId already minted");
         require(id >= idFloor, "tokenId below floor");
         require(!revokedIds[id], "tokenId revoked or burnt");
@@ -113,8 +138,8 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
 
     /**
      *  @notice Sets the price at which a token may be bought
-     *  @dev Setting a zero price cancels the sale (all prices are zero by default) 
-     *  @param id The token id 
+     *  @dev Setting a zero price cancels the sale (all prices are zero by default)
+     *  @param id The token id
      *  @param _price The token price in wei
      */
     function setPrice(uint256 id, uint256 _price) external {
@@ -125,17 +150,17 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
 
     /**
      *  @notice Returns the price at which a token may be bought
-     *  @dev A zero price means the token is not for sale 
-     *  @param id The token id 
+     *  @dev A zero price means the token is not for sale
+     *  @param id The token id
      */
-    function price(uint256 id) external view returns(uint256) {
+    function price(uint256 id) external view returns (uint256) {
         return prices[id];
     }
 
     /**
      *  @notice Transfers the token to the caller, transfers the paid ETH to its owner (minus any royalty)
-     *  @dev A zero price means the token is not for sale 
-     *  @param id The token id 
+     *  @dev A zero price means the token is not for sale
+     *  @param id The token id
      */
     function buy(uint256 id) external payable {
         require(_msgSender() != ownerOf(id), "caller is token owner");
@@ -144,7 +169,10 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
         address seller = ownerOf(id);
         delete prices[id];
         _safeTransfer(seller, _msgSender(), id, "");
-        Address.sendValue(payable(seller), (10000 - royaltyBasisPoints) * (msg.value / 10000));
+        Address.sendValue(
+            payable(seller),
+            (10000 - royaltyBasisPoints) * (msg.value / 10000)
+        );
         emit Bought(id, _msgSender());
     }
 
@@ -164,7 +192,10 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
      *  @param floor The floor for token Ids minted from now onward
      */
     function setIdFloor(uint256 floor) external {
-        require(hasRole(AGENT_ROLE, _msgSender()), "unauthorized to set idFloor");
+        require(
+            hasRole(AGENT_ROLE, _msgSender()),
+            "unauthorized to set idFloor"
+        );
         require(floor > idFloor, "must exceed current floor");
         idFloor = floor;
         IdFloorSet(idFloor);
@@ -181,14 +212,23 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
     /**
      *  @dev See {IERC165-supportsInterface}.
      */
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControl) returns (bool) {
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, AccessControl)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 
     /**
      * @dev Minting also increments totalSupply
      */
-    function _mint(address recipient, uint256 id, string memory uri) internal {
+    function _mint(
+        address recipient,
+        uint256 id,
+        string memory uri
+    ) internal {
         _safeMint(recipient, id);
         _setTokenURI(id, uri);
         totalSupply += 1;
@@ -197,13 +237,24 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
     /**
      * @dev Recreates the hash that the signer (may have) signed
      */
-    function _hash(uint256 weiPrice, uint256 id, string memory uri) internal view returns (bytes32) {
-        return _hashTypedDataV4(keccak256(abi.encode(
-            keccak256("mint(uint256 weiPrice,uint256 tokenId,string tokenURI)"),
-            weiPrice,
-            id,
-            keccak256(bytes(uri))
-        )));
+    function _hash(
+        uint256 weiPrice,
+        uint256 id,
+        string memory uri
+    ) internal view returns (bytes32) {
+        return
+            _hashTypedDataV4(
+                keccak256(
+                    abi.encode(
+                        keccak256(
+                            "mint(uint256 weiPrice,uint256 tokenId,string tokenURI)"
+                        ),
+                        weiPrice,
+                        id,
+                        keccak256(bytes(uri))
+                    )
+                )
+            );
     }
 
     /**
@@ -214,7 +265,7 @@ contract NFTsurface is ERC721, ERC721Burnable, EIP712, AccessControl, PaymentSpl
         tokenURIs[id] = uri;
     }
 
-     /**
+    /**
      * @dev burn a token and prevent the reuse of its Id
      */
     function _burn(uint256 id) internal override {
